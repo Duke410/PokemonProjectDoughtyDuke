b638f502c4637de06c932857efae0d2ea2dbbdc9
ndoughty4 <noradoughty@gmail.com>
2022-09-13 13:59:54 -0500

    Nora: basic structure of attack, player, and pokemon

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Player {
 
     String playerName;
+    int playerNumber;
     ArrayList<Pokemon> pokemonList = new ArrayList<Pokemon>();
 
     public Player(String playerName, int playerNumber){
@@ -14,8 +15,45 @@ public class Player {
     }
 
     public String getPlayerName(){
+        return playerName;
+    }
+    public void setPlayerName(){
+        this.playerName = playerName;
+    }
+    public int getPlayerNumber(){
+        return playerNumber;
+    }
+    public void setPlayerNumber(){
+        this.playerName = playerName;
+    }
+    public Pokemon getPokemonList(int index){
+        return pokemonList.get(index);
+    }
+
+
+    public void addPokemon(Pokemon newPokemon){
+        pokemonList.add(newPokemon);
+    }
+    public void subtractPokemon(Pokemon lostPokemon){
+        for(int i = 0; i<= pokemonList.size(); i++){
+            if(pokemonList.get(i).getName().equals(lostPokemon.getName())){
+                pokemonList.remove(i);
+            }
+        }
+    }
+    public Pokemon choosePokemon(){
 
     }
-    p
+    public Item chooseItem(){
+
+    }
+    public void runAway(){
+
+    }
+    public void ranChangePokemon(){
+
+    }
+
+
 
 }
