c5fa35b793dcafa022f3adb41ce10c95cb7c6d11
Duke410 <dmduke4@gmail.com>
2022-09-16 23:06:21 -0500

    couple little things

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public Game thisGame;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
